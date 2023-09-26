6595801120281aed6165a085cbcea6f3cc5ad8c1
Julie C <jucheng925@gmail.com>
2023-09-25 20:32:51 -0400

    owner-pet relationship

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,27 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    
+    def __init__(self, name, pet_type, owner = None):
+        self.name = name
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception("Pet type need to be in pet_types list")
+        self.owner = owner
+        Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Pet must be an instance of Pet class.")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(Pet.all, key =lambda p:p.name)
